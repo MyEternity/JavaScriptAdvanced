8e4f985253881c66530f05acb9a02abdf03fb5b7
MyEternity <spddarkforce47@gmail.com>
2021-09-20 10:33:17 +0300

    Some fixes

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -13,5 +13,5 @@ const renderGoodsList = (list) => {
     document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.title, item.price));
 }
 
-document.querySelector('.goods-list').innerHTML = "<p>Test</p>"
-// renderGoodsList(goods);
+//document.querySelector('.goods-list').innerHTML = "<p>Test</p>"
+ renderGoodsList(goods);
